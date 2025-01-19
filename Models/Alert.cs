ca4dde7091d1a876f26eb960b0050f3d5f61b01d
Lielbiton <88681473+Lielbiton@users.noreply.github.com>
2025-01-19 12:54:40 +0200

    Add files via upload

---
+++ Models/Alert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightManagementService.Models
+{
+    public class Alert
+    {
+        public string AlertId { get; set; }
+        public string Destination { get; set; }
+        public float MaxPrice { get; set; }
+        public float MinPrice { get; set; }
+        public string AlertType { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string FlightId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Services/IAlertManagementService.cs
@@ -0,0 +1,16 @@
+﻿using FlightManagementService.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightManagementService.Services
+{
+    public interface IAlertManagementService
+    {
+        void AddAlert(string userId, string destination, float maxPrice);
+        List<Alert> GetAlerts();
+        void UpdateAlert(string alertId, string newDestination, float newMaxPrice);
+        void DeleteAlert(string alertId);
+        List<Alert> GetAlertsByUserId(string userId);
+    }
+}
