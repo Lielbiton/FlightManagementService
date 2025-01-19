5d6f94b49a58cb74490c6e49d8e2933fe47a4bb0
Lielbiton <88681473+Lielbiton@users.noreply.github.com>
2025-01-19 12:57:29 +0200

    Add files via upload

---
+++ Services/IAlertManagementService.cs
@@ -0,0 +1,16 @@
+﻿using FlightManagementService.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightManagementService.Services
+{
+    public interface IAlertManagementService
+    {
+        void AddAlert(Alert alert);
+        List<Alert> GetAlerts();
+        void UpdateAlert(Alert alert);
+        void DeleteAlert(string alertId);
+        List<Alert> GetAlertsByUserId(string userId);
+    }
+}
